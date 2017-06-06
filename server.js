b8655365268bc6d768848f3d0c8a0eb52b96e909
mumu <455635414@qq.com>
2017-06-06 18:20:01 +0800

    change port

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.get('/api/data',function(req,res){
 
 let key = '1309DB1889014394F50D6775808A950B';
 let account = '136791661';
-app.listen('3000', function (err) {
+app.listen('80', function (err) {
   if (err) {
     return
   }
-  console.log("listen 3000")
+  console.log("listen 80")
 })
