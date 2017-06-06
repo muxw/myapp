929b8978db53f13fe1b3d19e7882412599959141
mumu <455635414@qq.com>
2017-06-06 19:46:00 +0800

    1

--- server.js
+++ server.js
@@ -21,9 +21,11 @@ app.get('/api/data',function(req,res){
 })
 app.post('/wechat',function(req,res){
   console.log(req.body);
+  let openId = req.body.xml.tousername[0];
+  let time = new Date.getTime();
   res.send(`<xml>
-<ToUserName><![CDATA[toUser]]></ToUserName>
-<FromUserName><![CDATA[fromUser]]></FromUserName>
+<ToUserName><![CDATA[${openId}]]></ToUserName>
+<FromUserName><![CDATA[oKuUSwH3uJ069brOZRnPRYbosc6g]]></FromUserName>
 <CreateTime>12345678</CreateTime>
 <MsgType><![CDATA[text]]></MsgType>
 <Content><![CDATA[你好]]></Content>
