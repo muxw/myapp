b9bd1ab568c7a505eb3047833b6a7b02f280226a
mxw <455635414@qq.com>
2017-06-20 21:27:33 +0800

    主题

--- router/index.js
+++ router/index.js
@@ -6,26 +6,33 @@ let superagent = require('superagent');
 router.get('/data', function (req, res) {
   res.json(hero)
 })
-router.get('/news' ,(req, res) => {
+router.get('/news', (req, res) => {
   superagent
     .get('http://news-at.zhihu.com/api/4/news/latest')
-    .end((err,msg) => {
+    .end((err, msg) => {
       res.json(msg.body)
     })
 })
-router.get('/before',(req,res) => {
+router.get('/before', (req, res) => {
   let date = req.query.date;
   superagent
-    .get('http://news-at.zhihu.com/api/4/news/before/'+date)
-    .end((err,msg) => {
+    .get('http://news-at.zhihu.com/api/4/news/before/' + date)
+    .end((err, msg) => {
       res.json(msg.body)
     })
 })
-router.get('/article/:id', (req,res) => {
+router.get('/article/:id', (req, res) => {
   let id = req.params.id;
   superagent
-    .get('https://news-at.zhihu.com/api/4/news/'+id)
-    .end((err,msg) => {
+    .get('https://news-at.zhihu.com/api/4/news/' + id)
+    .end((err, msg) => {
+      res.json(msg.body)
+    })
+})
+router.get('/themes', (req, res) => {
+  superagent
+    .get('https://news-at.zhihu.com/api/4/themes')
+    .end((err, msg) => {
       res.json(msg.body)
     })
 })
